17bb15cf5bb97fa8be0a60a48e3f2cc678f544ae
kign17019999 <purintanirat@hotmail.com>
2023-02-10 00:15:03 +0000

    1

--- read_CSR.c
+++ read_CSR.c
@@ -1,88 +1,103 @@
+#include "mmio.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "mmio.h"
 
-void read_matrix_csr(const char* file, int* I, int* J, double* val, int* n, int* m, int* nz) {
-  int ret_code;
+int main() {
   MM_typecode matcode;
-  FILE *f;
-
-  if ((f = fopen(file, "r")) == NULL)
-    return;
-
-  if (mm_read_banner(f, &matcode) != 0) {
-    printf("Could not process Matrix Market banner.\n");
-    return;
-  }
-
-  /*  This is how one can screen matrix types if their application */
-  /*  only supports a subset of the Matrix Market data types.      */
-  if (mm_is_complex(matcode) && mm_is_matrix(matcode) &&
-      mm_is_sparse(matcode) ) {
-    printf("Sorry, this application does not support ");
-    printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
-    return;
-  }
-
-  /* find out size of sparse matrix .... */
-  if ((ret_code = mm_read_mtx_crd_size(f, n, m, nz)) !=0)
-    return;
-
-  /* reseve memory for matrices */
-  I = (int *) malloc(*n + 1 * sizeof(int));
-  J = (int *) malloc(*nz * sizeof(int));
-  val = (double *) malloc(*nz * sizeof(double));
-
-  /* NOTE: when reading in doubles, ANSI C requires the use of the "l"  */
-  /*   specifier as in "%lg", "%lf", "%le", otherwise errors will occur */
-  /*  (ANSI C X3.159-1989, Sec. 4.9.6.2, p. 136 lines 13-15)        */
-
-  for (int i = 0; i < *n + 1; i++)
-    fscanf(f, "%d", &I[i]);
-
-  for (int i = 0; i < *nz; i++) {
-    fscanf(f, "%d", &J[i]);
-    fscanf(f, "%lf", &val[i]);
-  }
-
-  if (f !=stdin) fclose(f);
-
-  return;
-}
-
-int main(int argc, char *argv[]) {
-  int n, m, nz;
-  int *I, *J;
+  int M, N, nz;
+  int i, *I, *J;
   double *val;
 
-  if (argc != 2) {
-    printf("Usage: %s [martix-market-filename]\n", argv[0]);
-    return 0;
-  } else {
-    read_matrix_csr(argv[1], I, J, val, &n, &m, &nz);
+  // Read the matrix from file
+  FILE *f = fopen("matrix.mtx", "r");
+  if (mm_read_banner(f, &matcode) != 0) {
+    printf("Could not process Matrix Market banner.\n");
+    exit(1);
   }
 
+  // Check if the matrix is real (not complex)
+  if (!mm_is_real(matcode)) {
+    printf("Sorry, this example does not support complex matrices.\n");
+    exit(1);
+  }
+
+  // Check if the matrix is sparse
+  if (!mm_is_matrix(matcode)) {
+    printf("Sorry, this example only supports sparse matrices.\n");
+    exit(1);
+  }
+
+  // Get the size of the matrix
+  if (mm_read_mtx_crd_size(f, &M, &N, &nz) !=0) {
+    printf("Could not read matrix size.\n");
+    exit(1);
+  }
+
+  // Allocate memory for the arrays to store the matrix
+  I = (int *) malloc(nz * sizeof(int));
+  J = (int *) malloc(nz * sizeof(int));
+  val = (double *) malloc(nz * sizeof(double));
+
+  // Read the matrix from the file into the arrays
+  for (i=0; i<nz; i++) {
+    fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+    I[i]--;  /* adjust from 1-based to 0-based */
+    J[i]--;
+  }
+
+  // Close the file
+  if (f !=NULL) fclose(f);
+
+  // Print the matrix to verify that it was read correctly
+  printf("Matrix read from file:\n");
+  for (i=0; i<nz; i++)
+    printf("%d %d %g\n", I[i], J[i], val[i]);
+
+  // Convert the matrix to CSR format
+  int *row_ptr = (int *) malloc((M+1) * sizeof(int));
+  int *col_ind = (int *) malloc(nz * sizeof(int));
+  double *csr_val = (double *) malloc(nz * sizeof(double));
+  int idx = 0;
+  row_ptr[0] = 0;
+  for (i=0; i<M; i++) {
+    int row_nnz = 0;
+    while (I[idx] == i) {
+      row_nnz++;
+      idx++;
+    }
+    row_ptr[i+1] = row_ptr[i] + row_nnz;
+  }
+  idx = 0;
+  for (i=0; i<M; i++) {
+    int row_start = row_ptr[i];
+    int row_end = row_ptr[i+1];
+    for (int j=row_start; j<row_end; j++) {
+      col_ind[j] = J[idx];
+      csr_val[j] = val[idx];
+      idx++;
+    }
+  }
+
+  // Print the CSR representation of the matrix to verify
   printf("Matrix stored in CSR format:\n");
-  printf("Number of rows: %d\n", n);
-  printf("Number of columns: %d\n", m);
-  printf("Number of non-zero elements: %d\n", nz);
-  printf("Row indices (starts from 0):\n");
-  for (int i = 0; i < n + 1; i++)
-    printf("%d ", I[i]);
-  printf("\n");
-  printf("Column indices (starts from 0):\n");
-  for (int i = 0; i < nz; i++)
-    printf("%d ", J[i]);
-  printf("\n");
-  printf("Non-zero values:\n");
-  for (int i = 0; i < nz; i++)
-    printf("%.2lf ", val[i]);
+  printf("row_ptr: ");
+  for (i=0; i<=M; i++)
+    printf("%d ", row_ptr[i]);
+  printf("\ncol_ind: ");
+  for (i=0; i<nz; i++)
+    printf("%d ", col_ind[i]);
+  printf("\ncsr_val: ");
+  for (i=0; i<nz; i++)
+    printf("%g ", csr_val[i]);
   printf("\n");
 
-  /* free memory */
+  // Free memory
   free(I);
   free(J);
   free(val);
+  free(row_ptr);
+  free(col_ind);
+  free(csr_val);
 
   return 0;
 }
