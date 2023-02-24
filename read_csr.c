23410ef687c7065bad43eef1b8c0c0b93b38ba6c
kign17019999 <purintanirat@hotmail.com>
2023-02-24 23:42:07 +0000

    Update read_csr.c

--- read_csr.c
+++ read_csr.c
@@ -8,71 +8,48 @@ int read_csr_matrix(const char *file_name, struct csr_matrix *matrix) {
   int ret_code;
 
   FILE *f = fopen(file_name, "r");
-  if (!f) {
+  if (!f || mm_read_banner(f, &matcode) != 0 || mm_read_mtx_crd_size(f, &matrix->M, &matrix->N, &matrix->NNZ) != 0) {
+    if (f) fclose(f);
     return -1;
   }
 
-  if (mm_read_banner(f, &matcode) != 0) {
-    fclose(f);
-    return -1;
-  }
+  matrix->IRP = (int *) malloc((matrix->M + 1) * sizeof(int));
+  matrix->JA = (int *) malloc(matrix->NNZ * sizeof(int));
+  matrix->AZ = (double *) malloc(matrix->NNZ * sizeof(double));
+  matrix->IRP[0] = 0;
 
-  int M, N, NNZ;
-  if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &NNZ)) != 0) {
-    fclose(f);
-    return ret_code;
-  }
+  int *row_counts = (int *) calloc(matrix->M, sizeof(int));
 
-  matrix->M = M;
-  matrix->N = N;
-  matrix->NNZ = NNZ;
-
-  matrix->IRP = (int *) malloc((M + 1) * sizeof(int));
-  matrix->JA = (int *) malloc(NNZ * sizeof(int));
-  matrix->AZ = (double *) malloc(NNZ * sizeof(double));
-
-  int i;
-  for (i = 0; i <= M; i++) {
-    matrix->IRP[i] = 0;
-  }
-
-  int row_counts = 0;
-  int last_row = -1;
   int row, col;
   double AZ;
-  for (i = 0; i < NNZ; i++) {
-    if (fscanf(f, "%d %d", &row, &col) != 2) {
-      fclose(f);
-      return -1;
-    }
-    row--;
-    col--;
-
-    if (row != last_row) {
-      for (; last_row < row; last_row++) {
-        matrix->IRP[last_row + 1] = row_counts;
-      }
-    }
-
-    if (mm_is_pattern(matcode)) {
-      AZ = 1.0;
-    } else {
-      if (fscanf(f, "%lf", &AZ) != 1) {
-        fclose(f);
-        return -1;
-      }
-    }
-
-    matrix->JA[row_counts] = col;
-    matrix->AZ[row_counts] = AZ;
-    row_counts++;
+  for (int i = 0; i < matrix->NNZ; i++) {
+    if (fscanf(f, "%d %d", &row, &col) != 2) goto failure;
+    row--; col--;
+    if (!mm_is_pattern(matcode) && fscanf(f, "%lf", &AZ) != 1) goto failure;
+    row_counts[row]++;
   }
 
-  for (; last_row < M; last_row++) {
-    matrix->IRP[last_row + 1] = row_counts;
+  for (int i = 0; i < matrix->M; i++) matrix->IRP[i + 1] = matrix->IRP[i] + row_counts[i];
+  for (int i = 0; i < matrix->M; i++) row_counts[i] = 0;
+  rewind(f);
+
+  if (mm_read_banner(f, &matcode) != 0 || mm_read_mtx_crd_size(f, &matrix->M, &matrix->N, &matrix->NNZ) != 0) goto failure;
+
+  for (int i = 0; i < matrix->NNZ; i++) {
+    if (fscanf(f, "%d %d", &row, &col) != 2) goto failure;
+    row--; col--;
+    if (!mm_is_pattern(matcode) && fscanf(f, "%lf", &AZ) != 1) goto failure;
+    int index = matrix->IRP[row] + row_counts[row]++;
+    matrix->JA[index] = col;
+    matrix->AZ[index] = AZ;
   }
 
+  free(row_counts);
   fclose(f);
   return 0;
-}
 
+failure:
+  free(row_counts);
+  fclose(f);
+  return -1;
+}
