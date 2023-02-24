330ff612d6588bd8dce1b7128ae5eb3c6e64165b
kign17019999 <purintanirat@hotmail.com>
2023-02-24 23:51:12 +0000

    Update read_ellpack.c

--- read_ellpack.c
+++ read_ellpack.c
@@ -5,109 +5,59 @@
 
 int read_ellpack_matrix(const char *file_name, struct ellpack_matrix *matrix) {
   MM_typecode matcode;
-  int ret_code;
+  int ret_code, M, N, NNZ, row, col, *row_counts, i, j = 0;
+  double AZ;
 
   FILE *f = fopen(file_name, "r");
-  if (!f) {
+  if (!f || mm_read_banner(f, &matcode) != 0 || (ret_code = mm_read_mtx_crd_size(f, &M, &N, &NNZ)) != 0) {
+    if (f) fclose(f);
     return -1;
   }
 
-  if (mm_read_banner(f, &matcode) != 0) {
-    fclose(f);
-    return -1;
-  }
-
-  int M, N, NNZ;
-  if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &NNZ)) != 0) {
-    fclose(f);
-    return ret_code;
-  }
-
-  int i, j;
   matrix->M = M;
   matrix->N = N;
   matrix->NNZ = NNZ;
 
-  int row, col;
-  double AZ;
-  int *row_counts = (int *) calloc(M, sizeof(int));
+  row_counts = (int *) calloc(M, sizeof(int));
+  matrix->MAXNZ = 0;
 
-  for (i = 0; i < NNZ; i++) {
-    if (fscanf(f, "%d %d", &row, &col) != 2) {
+  for (i = 0; i < NNZ && fscanf(f, "%d %d", &row, &col) == 2; i++) {
+    row--, col--;
+
+    if (!mm_is_pattern(matcode) && fscanf(f, "%lf", &AZ) != 1) {
       fclose(f);
+      free(row_counts);
       return -1;
     }
-    row--;
-    col--;
 
-    if (mm_is_pattern(matcode)) {
-      AZ = 1.0;
-    } else {
-      if (fscanf(f, "%lf", &AZ) != 1) {
-        fclose(f);
-        return -1;
-      }
-    }
-
-    row_counts[row]++;
-  }  
-
-  matrix->MAXNZ = 0;
-  for (i = 0; i < M; i++) {
-    if (row_counts[i] > matrix->MAXNZ) {
-      matrix->MAXNZ = row_counts[i];
-    }
+    row_counts[row]++, matrix->MAXNZ = row_counts[row] > matrix->MAXNZ ? row_counts[row] : matrix->MAXNZ;
   }
 
   matrix->JA = (int *) malloc(M * matrix->MAXNZ * sizeof(int));
   matrix->AZ = (double *) malloc(M * matrix->MAXNZ * sizeof(double));
-
-  for (i = 0; i < M; i++) {
-    for (j = 0; j < matrix->MAXNZ; j++) {
-      matrix->JA[i * matrix->MAXNZ + j] = -1;
-    }
-  }
+  for (i = 0; i < M * matrix->MAXNZ; i++) matrix->JA[i] = -1;
 
   rewind(f);
-
-  if (mm_read_banner(f, &matcode) != 0) {
+  if (mm_read_banner(f, &matcode) != 0 || (ret_code = mm_read_mtx_crd_size(f, &M, &N, &NNZ)) != 0) {
     fclose(f);
+    free(row_counts);
     return -1;
   }
 
-  if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &NNZ)) != 0) {
-    fclose(f);
-    return ret_code;
-  }
-  
-  for (i = 0; i < NNZ; i++) {
-    if (fscanf(f, "%d %d", &row, &col) != 2) {
+  for (i = 0; i < NNZ && fscanf(f, "%d %d", &row, &col) == 2; i++) {
+    row--, col--;
+
+    if (!mm_is_pattern(matcode) && fscanf(f, "%lf", &AZ) != 1) {
       fclose(f);
+      free(row_counts);
       return -1;
     }
-    row--;
-    col--;
 
-    if(mm_is_pattern(matcode)) {
-      AZ = 1.0;
-    } else {
-      if (fscanf(f, "%lf", &AZ) != 1) {
-        fclose(f);
-        return -1;
-      }    
-    }
-    int index = row * matrix->MAXNZ;
-    for (j = 0; j < matrix->MAXNZ; j++) {
-      if (matrix->JA[index + j] == -1) {
-        matrix->JA[index + j] = col;
-        matrix->AZ[index + j] = AZ;
-        break;
-      }
-    }
-  }  
+    while (matrix->JA[row * matrix->MAXNZ + j] != -1) j++;
+    matrix->JA[row * matrix->MAXNZ + j] = col, matrix->AZ[row * matrix->MAXNZ + j] = AZ;
+  }
 
   fclose(f);
   free(row_counts);
   return 0;
 }
-
